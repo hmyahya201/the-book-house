888f0d281bdc377a7ee273d2464674c138b0bc8b
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-05-28 16:53:53 +0600

    added book single page

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,16 +1,66 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const BookDetails = () => {
     const book = useLoaderData();
-    const {authors} = book
+    const [fold, setFold] = useState(true)
+    const { image, title, desc, authors, publisher, year, rating, url, price } = book
     console.log(book)
     return (
-        <div>
-            <h2>Book Details</h2>
-            <div>
-                <p>{authors}</p>
+        <div className='container mx-auto p-5'>
+            <div className='flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto'>
+                {/* Image Container */}
+                <div className=' lg:w-1/2 h-full'>
+                    <img
+                        src={image}
+                        alt='book cover'
+                        className='object-cover w-full  lg:h-full'
+                    />
+                </div>
+                {/* Details Container */}
+                <div className=' p-8 bg-white lg:p-16 lg:pl-10 lg:w-1/2'>
+                    <div>
+                        <p className='badge'>Brand new</p>
+                    </div>
+                    <h5 className='mb-3 text-3xl font-extrabold leading-none sm:text-4xl'>
+                        {title}
+                    </h5>
+                    <p className=' text-gray-900'>Authors: {authors.substring(0, 50)}</p>
+                    <p className=' text-gray-900'>Publisher: {publisher}</p>
+                    <p className=' text-gray-900'>Year: {year}</p>
+                    <p className='mb-5 text-gray-900'>Rating: {rating}</p>
+                    {fold ? (
+                        <>
+                        <p className=' text-gray-500'>{desc.substring(0, 100)}.....</p>
+                        <span
+                            className='cursor-pointer text-blue-600 '
+                            onClick={() => setFold(!fold)}
+                        >
+                            Read More
+                        </span>
+                        </>
+                    ) : (
+                        <>
+                        <p className=' text-gray-900'>{desc}.....</p>
+                        <span
+                            className='cursor-pointer text-blue-600 '
+                            onClick={() => setFold(!fold)}
+                        >
+                            Read Less
+                        </span>
+                        </>
+                    )}
+
+                    <div className='flex gap-5 mt-8 items-center'>
+                        <Link to={url} target='_blank' className='btn'>
+                        Buy Now
+                        </Link>
+                        <p className='items-center font-extrabold text-gray-600 '>
+                        Price: {price}
+                        </p>
+                </div>
+                </div>
             </div>
         </div>
     );
